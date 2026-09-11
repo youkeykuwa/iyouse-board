181f883600947b5a0a80b2e3fca42476e7785858
youkeykuwa <108604058+youkeykuwa@users.noreply.github.com>
2026-09-12 01:57:41 +0300

    worker.jsつくる

---
+++ worker.js
@@ -0,0 +1,42 @@
+Skip to content
+youkeykuwa
+iyouse-board
+Repository navigation
+Code
+Issues
+Pull requests
+Agents
+Actions
+Projects
+Wiki
+Security and quality
+Insights
+Settings
+Files
+Go to file
+t
+T
+index.html
+iyouse-board
+/
+worker.js
+in
+main
+
+Edit
+
+Preview
+Indent mode
+
+Spaces
+Indent size
+
+2
+Line wrap mode
+
+No wrap
+Editing worker.js file contents
+  1
+
+Use Control + Shift + m to toggle the tab key moving focus. Alternatively, use esc then tab to move to the next interactive element on the page.
+ 
